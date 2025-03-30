fd74fe8d3b4ae630e5f868c7515defbaf7b10c5c
darrl <darrleene@gmail.com>
2025-03-29 22:50:59 -0300

    Initial commit

---
+++ .venv/main.py
@@ -0,0 +1,14 @@
+import pygame
+
+print('Setup Start') #Aparece ae
+pygame.init() #necessita dessa inicialização
+window = pygame.display.set_mode(size=(600, 450)) #UMA JANELA TENTA SER ABERTA, PORÉM FECHA, PROGRAMA A ENCERRA
+print('Setup End')
+print('Loop Start')
+while True: #loop de laço infinito, fica preso para sempre
+   # pass/bug na tela, sendo necessário configurar botão de fechar a janela, trabalhando com eventos.
+#check for all events
+    for event in pygame.event.get(): #pegar os eventos e ficar checando
+        if event. type == pygame.QUIT:
+            pygame.quit() #close windown
+            quit() # end game
